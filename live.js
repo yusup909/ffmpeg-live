88059cc3843854ad402d5eb562ace7ba951260d0
YusupKakuu <83402322+yusup909@users.noreply.github.com>
2023-03-04 15:17:02 +0700

    Create live.js

---
+++ live.js
@@ -0,0 +1,33 @@
+// This code will stream a YouTube video to a local RTMP server using FFmpeg.
+
+const { spawn } = require('child_process');
+
+const url = 'https://www.youtube.com/watch?v=YOUR_VIDEO_ID'; // Replace with your YouTube video URL
+
+const rtmpUrl = 'rtmp://localhost/live/stream'; // Replace with your RTMP server URL
+
+const ffmpeg = spawn('ffmpeg', [
+
+  '-i', url,
+
+  '-acodec', 'copy',
+
+  '-vcodec', 'copy',
+
+  '-f', 'flv',
+
+  rtmpUrl,
+
+]);
+
+ffmpeg.stdout.on('data', (data) => {
+
+  console.log(`stdout: ${data}`);
+
+});
+
+ffmpeg.stderr.on('data', (data) => {
+
+  console.log(`stderr: ${data}`);
+
+});
